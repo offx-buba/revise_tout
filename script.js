ed04d6f2f93cc6644dc799dce1366552163c5bed
offx-buba <msarr9726@gmail.com>
2025-08-19 22:36:45 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+// le code du  menu burger  
+
+const burger = document.getElementById('burger');
+const navLinks = document.getElementById('navbar-links');
+
+burger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
